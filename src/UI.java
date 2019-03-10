0b23be64c34b51518f0c91fb8f68d3077138657f
nerminemohsen <nerminemohsenarafa@gmail.com>
2019-03-11 00:12:34 +0200

    fixed small bug in ui.java class

--- src/UI.java
+++ src/UI.java
@@ -114,38 +114,3 @@ public class UI {
 
 
 }
-
-/*
-*
-* import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class ui {
-    private JTextArea chat;
-    private JPanel panel;
-    private JTextField message;
-    private JButton send;
-    public ui(){
-        PeerClass a=new PeerClass();
-        send.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                a.send(message.getText());
-            }
-        });
-
-    }
-
-
-
-    public static void main(String[] args) {
-        JFrame frame=new JFrame("ui");
-        frame.setContentPane(new ui().panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-    }
-}
-
-* */
